9abaf13bf392bf46305e022ee512492b72e22707
Ayrton Souza <ayrtonsouza@icloud.com>
2025-09-29 12:49:45 -0300

    [:construction:] giving examples for the different methods in a request

--- src/server.js
+++ src/server.js
@@ -14,6 +14,34 @@ import http from 'node:http';
 */
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  /*
+    Methods are a semantic way to identify the type of request.
+
+    GET => retrieve data from the server.
+    POST => send data to the server.
+    PUT => update data from a domain on the server.
+    DELETE => delete data from the server.
+    PATCH => update one specific data from a domain on the server.
+  */
+
+  if (method === 'GET' && url === '/users') {
+    return response.end('List of users');
+  }
+  if (method === 'POST' && url === '/users') {
+    return response.end('Create a user');
+  }
+  if (method === 'PUT' && url === '/users') {
+    return response.end('Update a user data as a whole');
+  }
+  if (method === 'DELETE' && url === '/users') {
+    return response.end('Delete a user');
+  }
+  if (method === 'PATCH' && url === '/users') {
+    return response.end('Update a specific data from a user');
+  }
+
   return response.end('Hello World!');
 });
 
